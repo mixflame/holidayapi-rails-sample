10119cbca0d65c5f641891db1030430c7b3a6f38
Jonathan Silverman <jsilverman2@gmail.com>
2020-10-20 17:00:30 -0600

    deliver 2019 holidays in debugged response (on index view)

--- app/controllers/holiday_controller.rb
+++ app/controllers/holiday_controller.rb
@@ -1,9 +1,26 @@
+require 'ruby-holidayapi'
+
 class HolidayController < ApplicationController
 
 
     def index
+        hapi = HolidayAPI::V1.new('24cbebb4-e8de-426f-a68d-fe6032446dfd')
 
+        parameters = {
+          # Required
+          'country' => 'US',
+          'year'    => 2019,
+          # Optional
+          # 'month'    => 7,
+          # 'day'      => 4,
+          # 'previous' => true,
+          # 'upcoming' => true,
+          # 'public'   => true,
+          # 'pretty'   => true,
+        }
+        
+        @response = hapi.holidays(parameters)
     end
 
-    
+
 end
