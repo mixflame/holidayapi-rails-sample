8ba5a7197d9934cd67852813ca526dcdfc704131
Jonathan Silverman <jsilverman2@gmail.com>
2020-10-20 18:19:01 -0600

    add remaining functionality and rely on my version of holidayapi-ruby gem (from github)

--- app/controllers/holiday_controller.rb
+++ app/controllers/holiday_controller.rb
@@ -5,25 +5,56 @@ class HolidayController < ApplicationController
 
     def index
 
+        hapi = HolidayAPI::V1.new('24cbebb4-e8de-426f-a68d-fe6032446dfd')
+
+        @response = hapi.countries
+
+        @countries = @response["countries"]
+
     end
 
     def holidays
         hapi = HolidayAPI::V1.new('24cbebb4-e8de-426f-a68d-fe6032446dfd')
 
+        @country = params[:country]
+
         parameters = {
-          # Required
-          'country' => params[:country],
+          'country' => @country,
           'year'    => 2019,
-          # Optional
-          # 'month'    => 7,
-          # 'day'      => 4,
-          # 'previous' => true,
-          # 'upcoming' => true,
-          # 'public'   => true,
-          # 'pretty'   => true,
         }
         
         @response = hapi.holidays(parameters)
+
+
+
+
+    end
+
+    def languages
+        hapi = HolidayAPI::V1.new('24cbebb4-e8de-426f-a68d-fe6032446dfd')
+
+        parameters = {}
+
+        @response = hapi.languages(parameters)
+
+    end
+
+    def workday
+        hapi = HolidayAPI::V1.new('24cbebb4-e8de-426f-a68d-fe6032446dfd')
+
+        
+        @country = params[:country]
+        @start = params[:start]
+        @days = params[:days]
+
+        parameters = {
+          'country' => @country,
+          'start' => @start,
+          'days' => @days
+        }
+        
+        @response = hapi.workday(parameters)
+
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "holiday#index"
   get "/holidays", to: "holiday#holidays"
+  get "/languages", to: "holiday#languages"
+  get "/workday", to: "holiday#workday"
 end
